8d69323fa70a56defcd1ed73a1955f68403fca48
John Lee <johnjun.lee91@gmail.com>
2020-03-22 20:34:11 -0400

    Create axios helper function to interact with Upsplash api

---
+++ src/api/unsplash.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import {API_KEY} from '../config.js';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: `Client-ID ${API_KEY}`
+    }
+});
